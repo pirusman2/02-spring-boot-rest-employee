7f34b576adb14364dac23173f145edb73f680c4e
pirusman2 <pirusman555@gmail.com>
2026-01-12 13:08:56 +0500

    Initial commit

---
+++ src/test/java/com/peercode/springboot/demo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peercode.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
